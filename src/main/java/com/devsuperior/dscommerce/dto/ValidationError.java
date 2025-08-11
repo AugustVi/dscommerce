7c53846a9ea5c13736fc817392dedf8046854a4a
AugustVi <augustovinicius221@gmail.com>
2025-08-10 23:57:19 -0300

    fix validation

--- src/main/java/com/devsuperior/dscommerce/dto/ValidationError.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ValidationError.java
@@ -8,7 +8,6 @@ public class ValidationError extends CustomError{
 
     private List<FieldMessage> errors = new ArrayList<>();
 
-
     public ValidationError(Instant timestamp, Integer status, String error, String path) {
         super(timestamp, status, error, path);
     }
@@ -18,6 +17,7 @@ public class ValidationError extends CustomError{
     }
 
     public void addError(String fieldName, String message){
+        errors.removeIf(x -> x.getFieldName().equals(fieldName));
         errors.add(new FieldMessage(fieldName, message));
     }
 }
